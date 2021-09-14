e36a977057761b2d9f407e68c68e50038c33a22d
KartikCD <chawdakartik@gmail.com>
2021-09-15 02:47:45 +0530

    toolbar setup

--- app/src/main/java/com/ammiskitchen/ammiskitchenapp/MainActivity.kt
+++ app/src/main/java/com/ammiskitchen/ammiskitchenapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ammiskitchen.ammiskitchenapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toolbar
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -18,11 +19,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
          super.onCreate(savedInstanceState)
-
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setTheme(R.style.AppTheme)
         setContentView(binding.root)
-
+        setSupportActionBar(binding.toolbar)
         val navController = findNavController(R.id.main_host_fragment)
     }
 
