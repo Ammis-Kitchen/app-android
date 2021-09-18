6181808b295f4b90ba91d0961c5600297980eb7a
KartikCD <chawdakartik@gmail.com>
2021-09-18 13:14:53 +0530

    cuisines and sub cuisines ui and adapter added

---
+++ app/src/main/java/com/ammiskitchen/ammiskitchenapp/presentation/ui/mainfeed/menu/adapter/CuisinesListAdapter.kt
@@ -0,0 +1,44 @@
+package com.ammiskitchen.ammiskitchenapp.presentation.ui.mainfeed.menu.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.ammiskitchen.ammiskitchenapp.databinding.ListCuisinesBinding
+
+class CuisinesListAdapter: ListAdapter<String, CuisinesListAdapter.CuisinesListViewHolder>(CuisineDiffUtilCallback) {
+
+    inner class CuisinesListViewHolder(
+        val binding: ListCuisinesBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(cuisine: String) {
+            binding.apply {
+                buttonCategory.text = cuisine
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CuisinesListViewHolder {
+        val binding = ListCuisinesBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return CuisinesListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CuisinesListViewHolder, position: Int) {
+        getItem(position)?.let {
+            holder.bind(it)
+        }
+    }
+}
+
+object CuisineDiffUtilCallback: DiffUtil.ItemCallback<String>() {
+    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+
+}
